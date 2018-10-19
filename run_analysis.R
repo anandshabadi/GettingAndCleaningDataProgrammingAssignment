bf809879482bc8e214450baa3917d727a10052d1
Anand Shabadi <anandshabadi@gmail.com>
2018-10-18 20:35:45 -0400

    shdeo

--- run_analysis.R
+++ run_analysis.R
@@ -12,6 +12,7 @@ if (!file.exists("UCI HAR Dataset")) {
 }
 
 #Adding this line to test GIT features
+#Ading more GIT features
 #Creating a branch from master
 # Load activity labels + features
 activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
